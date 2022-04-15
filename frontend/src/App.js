233a8b6ee8db3262aa6bf95647d85c9e1dea6dbf
Cyanjiner <jzheng@wesleyan.edu>
2022-04-14 21:12:36 -0400

    add login url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
           {/* <Route path="/" element={<Dashboard />} /> */}
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
