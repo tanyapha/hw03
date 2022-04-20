7c5db4a4fac2fee8ccd433f2ef12d1b82a426632
tanyapha <51808830+tanyapha@users.noreply.github.com>
2022-04-20 19:04:05 -0400

    changed host name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['song-rater.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://songrater-comp333.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
